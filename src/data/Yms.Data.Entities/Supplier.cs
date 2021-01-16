605d69a6923816630dd2e516f122fea915ff1a84
Can PERK <canperk@outlook.com>
2021-01-16 13:45:40 +0300

    supplier entitysi eklendi

---
+++ src/data/Yms.Data.Entities/Supplier.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yms.Data.Entities
+{
+    public class Supplier
+    {
+        public string Name { get; set; }
+    }
+}
