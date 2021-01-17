577fe69a03958a5e73629017047fceb828ffba54
Can PERK <canperk@outlook.com>
2021-01-17 11:12:09 +0300

    Temel projeler projeye eklendi

---
+++ src/data/Yms.Data.Entity.Abstractions/EntityBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Yms.Data.Entity.Abstractions
+{
+    public class EntityBase : IEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime Created { get; set; }
+        public Guid CreatedBy { get; set; }
+        public DateTime Updated { get; set; }
+        public Guid UpdatedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
