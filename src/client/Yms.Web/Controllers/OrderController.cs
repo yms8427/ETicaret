e86921a5202e8857fc47f9f1a9ca8446d486f750
Can PERK <canperk@outlook.com>
2021-01-23 12:07:19 +0300

    sayfa giydirme işlemi ve site içeriği tekrar eklendi. Önceki şablon bootstrap v3 olduğu için silindi

---
+++ src/client/Yms.Web/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Yms.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Invoice()
+        {
+            return View();
+        }
+    }
+}
