6e5da5135f09fcf87335fefb4c6531ae83e0ee3c
Can PERK <canperk@outlook.com>
2021-01-30 14:18:36 +0300

    middleware check

---
+++ src/api/Yms.Api/Middlewares/CheckHeaderMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Yms.Api.Middlewares
+{
+    public class CheckHeaderMiddleware
+    {
+        private readonly RequestDelegate next;
+        public CheckHeaderMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (!httpContext.Request.Headers.ContainsKey("X-Client-Type"))
+            {
+                httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+                await httpContext.Response.WriteAsync("Unknown client");
+                return;
+            }
+
+            await next(httpContext);
+        }
+    }
+}
